9feb8fea5c1049a2383c4792ecee33d5ded1c679
Andrey Esaulov <esaulov.andrey@gmail.com>
2021-08-29 18:08:50 +0200

    shop page, collection preview and collection item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import { Switch, Route, Link } from "react-router-dom";
+import { Switch, Route } from "react-router-dom";
+
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from "./components/shop/shop.component";
 
 function App() {
   return (
       <div>
           <Switch>
               <Route exact path='/' component={Homepage} />
+              <Route exact path='/shop' component={ShopPage} />
           </Switch>
       </div>
   );
